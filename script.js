d1f9f03b432919d8f619383a235c28a88e8ac9bf
Saba Komladze <121091520+SabaKomladze@users.noreply.github.com>
2023-04-28 15:56:46 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -7,19 +7,20 @@ const price = document.getElementsByClassName("number");
 const monthlyPriceOne = price[0].innerHTML;
 const monthlyPriceTwo = price[1].innerHTML;
 const monthlyPriceThree = price[2].innerHTML;
-
+const monthtofyear = document.getElementsByClassName("monthtofyear");
+const monthtofyearsecond = document.getElementsByClassName(
+  "monthtofyear-second"
+);
 burger.addEventListener("click", function () {
   navbar.style.display = "block";
   burger.style.display = "none";
   close.style.display = "block";
 });
-
 close.addEventListener("click", function () {
   navbar.style.display = "none";
   burger.style.display = "block";
   close.style.display = "none";
 });
-
 toggle.addEventListener("change", function changePrice() {
   if (isYearly == false) {
     isYearly = true;
@@ -28,17 +29,20 @@ toggle.addEventListener("change", function changePrice() {
   }
   makeYearly();
 });
-
-function changePrice() {}
-
 function makeYearly() {
   if (isYearly) {
-    price[0].innerHTML *= 10;
-    price[1].innerHTML *= 10;
-    price[2].innerHTML *= 10;
+    for (let i = 0; i < price.length; i++) {
+      price[i].innerHTML *= 10;
+    }
+    monthtofyear[0].innerHTML = "year";
+    monthtofyear[1].innerHTML = "year";
+    monthtofyearsecond[0].innerHTML = "year";
   } else {
     price[0].innerHTML = monthlyPriceOne;
     price[1].innerHTML = monthlyPriceTwo;
     price[2].innerHTML = monthlyPriceThree;
+    monthtofyear[0].innerHTML = "month";
+    monthtofyear[1].innerHTML = "month";
+    monthtofyearsecond[0].innerHTML = "month";
   }
 }
